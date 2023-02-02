939c9b105e810455cbeb89c04b6ea5f468f493cb
Joselio <jchabra595r@g.educaand.es>
2023-02-03 00:25:08 +0100

    Night mode disabled

--- app/src/main/java/com/politecnicomalaga/sixthdice/MainActivity.java
+++ app/src/main/java/com/politecnicomalaga/sixthdice/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         //Convoco dos tiradas cuales cada una tiene dos dados
         Jugador jugador1 = new Jugador();
